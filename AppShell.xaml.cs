c8047cc21883e54474a92f6b3d2c148993305674
vshak <vshak@DESKTOP-LR0MRF4>
2023-07-11 13:20:47 +0300

    Добавьте файлы проекта.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SeriesTracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SeriesTracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
